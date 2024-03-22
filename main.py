7898d10ce61626d5730c359eac2aecc21b2c24a5
Suryaansh-Jain <suryaansh2003@gmail.com>
2024-03-22 23:54:04 +0530

    fixed small bug

--- main.py
+++ main.py
@@ -10,6 +10,23 @@ client = anthropic.Anthropic(
 
 file_names = []
 
+def make_call(messgaes, system_data = None):
+    if (system_data == None):
+        return client.messages.create(
+            model="claude-3-opus-20240229",
+            max_tokens=1000,
+            temperature=0,
+            messages=messgaes
+        )
+              
+    return client.messages.create(
+        model="claude-3-opus-20240229",
+        max_tokens=1000,
+        temperature=0,
+        system=system_data,
+        messages=messgaes
+    )
+
 def summarize_documents():
     summaries = []
     for filename in os.listdir(DATA_DIR):
@@ -24,27 +41,22 @@ def summarize_documents():
 
             with open(pdf_path, 'rb') as file:
                 reader = PdfReader(file)
-                number_of_pages = len(reader.pages)
                 text = ''.join([page.extract_text() for page in reader.pages])
-                message = client.messages.create(
-                    model="claude-3-opus-20240229",
-                    max_tokens=1000,
-                    temperature=0,
-                    messages=[
-                        {
-                            "role": "user",
-                            "content": [
-                                {
-                                    "type": "text",
-                                    "text": f"Summarize this document in words: {text}"
-                                }
-                            ]
-                        }
-                    ]
-                )
+                command = [
+                    {
+                        "role": "user",
+                        "content": [
+                            {
+                                "type": "text",
+                                "text": f"Summarize this document in words: {text}"
+                            }
+                        ]
+                    }
+                ]
 
-                res = message.content
-                summaries.append(res[0].text)
+                response = make_call(command)
+                response = response.content
+                summaries.append(response[0].text)
                 with open(f"./summary/summary_{filename}.txt", "w") as file:
                     file.write(res[0].text)
 
@@ -58,7 +70,6 @@ def create_context(rel_index, file_names):
         file_name = file_names[index]
         with open(file_name, 'rb') as file:
             reader = PdfReader(file)
-            number_of_pages = len(reader.pages)
             text = ''.join([page.extract_text() for page in reader.pages])
 
             context += text
@@ -66,14 +77,14 @@ def create_context(rel_index, file_names):
     return context
 
 summaries = summarize_documents()
-messages = []
+conversation = []
 
 while True:
     query = input("Ask a question: ")
-    if query == "no":
+    if query == "quit":
         break
 
-    messages.append(
+    conversation.append(
         {
             "role": "user",
             "content": [
@@ -85,32 +96,28 @@ while True:
         }
     )
 
-    message = client.messages.create(
-        model="claude-3-opus-20240229",
-        max_tokens=1000,
-        temperature=0,
-        system=f"This is a list of summaries, {summaries}.",
-        messages=[
-            {
-                "role": "user",
-                "content": [
-                    {
-                        "type": "text",
-                        "text": f"Find the indices of the top 5 most relevant summaries for this question: {query}. Only output the list of (index, score) with 0 indexing and no other text, only the list with no other english words."
-                    }
-                ]
-            }
-        ]
-    )
 
-    res = message.content
-    res = res[0].text
+    system_message = f"This is a list of summaries, {summaries}."
+    command = [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": f"Find the indices of the top 5 most relevant summaries for this question: {query}. Only output the list of (index, score) with 0 indexing and no other text, only the list with no other english words."
+                }
+            ]
+        }
+    ]
+    best_docs = make_call(command, system_message).content
+    # best_docs = best_docs.content
+    best_docs = eval(best_docs[0].text)
 
-    res = eval(res)
+    # best_docs = eval(best_docs)
 
     best_indices = []
 
-    for index, score in res:
+    for index, score in best_docs:
         if score > 0.1:
             best_indices.append(index)
 
@@ -118,19 +125,14 @@ while True:
 
     best_context = create_context(best_indices, file_names)
 
-    message = client.messages.create(
-        model="claude-3-opus-20240229",
-        max_tokens=1000,
-        temperature=0,
-        system=f"This is some relevent context: {best_context}. Answer the questions based on this context. If you fell that the context is not relevent then ignore it. If there is any issue then return an empty list but never any other text.",
-        messages=messages
-    )
+    system_message = f"This is some relevent context: {best_context}. Answer the questions based on this context. If you fell that the context is not relevent then ignore it. If there is any issue then return an empty list but never any other text."
+    response = make_call(conversation, system_message)
 
-    res = message.content
-    print(res[0].text)
-    messages.append(
+    response = response.content
+    print(response[0].text)
+    conversation.append(
         {
             "role": "assistant",
-            "content": res[0].text
+            "content": response[0].text
         }
     )
